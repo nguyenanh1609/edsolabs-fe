e4257c5706018e9b7a8fe807e3455052dbdce791
nguyenanh1609 <nguyenhuuanh102000@gmail.com>
2021-09-08 15:08:08 +0700

    day22

--- Day 18-19/QuanTM/js/index.js
+++
@@ -1,8 +0,0 @@
-const heroContent = document.querySelector(".hero__content");
-const heroImg = document.querySelector(".hero__image");
-
-window.addEventListener("DOMContentLoaded", () => {
-  [heroContent, heroImg].forEach((item) => {
-    item.classList.add("animate__animated", "animate__backInUp");
-  });
-});
